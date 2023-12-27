9f8c0ee8d8c14d5e3a0a2674b6b80ddcf45e4015
DatYuUxie <118263835+DatYuUxie@users.noreply.github.com>
2023-12-27 16:09:49 +0700

    homepage1,2,3,4

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Onboarding2 from "./src/pages/onboarding2";
 import Onboarding1 from "./src/pages/onboarding1";
 import Onboarding3 from "./src/pages/onboarding3";
+import HomePage from "./src/pages/HomePage/index";
+import SeeAll from "./src/pages/SeeAll";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Onboarding1" screenOptions={{headerShown:false}}>
+    <Stack.Navigator initialRouteName="HomePage" screenOptions={{headerShown:false}}>
       <Stack.Group>
-        <Stack.Screen name="Onboarding1" component={Onboarding1} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="SeeAll" component={SeeAll} />
+        {/* <Stack.Screen name="Onboarding1" component={Onboarding1} />
         <Stack.Screen name="Onboarding2" component={Onboarding2} />
-        <Stack.Screen name="Onboarding3" component={Onboarding3} />
+        <Stack.Screen name="Onboarding3" component={Onboarding3} /> */}
       </Stack.Group>
     </Stack.Navigator>
      </NavigationContainer>
